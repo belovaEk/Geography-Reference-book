8e457413c6f65a6bf03142e24ea5bc33d4d4c58f
Tyuiu.BelovaEA@outlook.com <belova.ek0@gmail,com>
2023-12-16 19:05:34 +0500

    Add project files.

---
+++ Tyuiu.BelovaEA.Sprint7.Project.V13/FormMainMenu.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace Tyuiu.BelovaEA.Sprint7.Project.V13
+{
+    partial class FormMainMenu_BEA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMainMenu_BEA));
+            this.panelMenu_BEA = new System.Windows.Forms.Panel();
+            this.panelLogo_BEA = new System.Windows.Forms.Panel();
+            this.buttonGeography_BEA = new System.Windows.Forms.Button();
+            this.buttonNature_BEA = new System.Windows.Forms.Button();
+            this.buttonPopulation_BEA = new System.Windows.Forms.Button();
+            this.buttonEconomy_BEA = new System.Windows.Forms.Button();
+            this.buttonPolitics_BEA = new System.Windows.Forms.Button();
+            this.buttonFact_BEA = new System.Windows.Forms.Button();
+            this.panelTitleBar_BEA = new System.Windows.Forms.Panel();
+            this.labelTitle_BEA = new System.Windows.Forms.Label();
+            this.panelMenu_BEA.SuspendLayout();
+            this.panelTitleBar_BEA.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu_BEA
+            // 
+            this.panelMenu_BEA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelMenu_BEA.Controls.Add(this.buttonFact_BEA);
+            this.panelMenu_BEA.Controls.Add(this.buttonPolitics_BEA);
+            this.panelMenu_BEA.Controls.Add(this.buttonEconomy_BEA);
+            this.panelMenu_BEA.Controls.Add(this.buttonPopulation_BEA);
+            this.panelMenu_BEA.Controls.Add(this.buttonNature_BEA);
+            this.panelMenu_BEA.Controls.Add(this.buttonGeography_BEA);
+            this.panelMenu_BEA.Controls.Add(this.panelLogo_BEA);
+            this.panelMenu_BEA.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu_BEA.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu_BEA.Name = "panelMenu_BEA";
+            this.panelMenu_BEA.Size = new System.Drawing.Size(220, 644);
+            this.panelMenu_BEA.TabIndex = 0;
+            // 
+            // panelLogo_BEA
+            // 
+            this.panelLogo_BEA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panelLogo_BEA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo_BEA.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo_BEA.Name = "panelLogo_BEA";
+            this.panelLogo_BEA.Size = new System.Drawing.Size(220, 80);
+            this.panelLogo_BEA.TabIndex = 0;
+            // 
+            // buttonGeography_BEA
+            // 
+            this.buttonGeography_BEA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonGeography_BEA.FlatAppearance.BorderSize = 0;
+            this.buttonGeography_BEA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGeography_BEA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGeography_BEA.ForeColor = System.Drawing.Color.Gainsboro;
+            this.buttonGeography_BEA.Image = ((System.Drawing.Image)(resources.GetObject("buttonGeography_BEA.Image")));
+            this.buttonGeography_BEA.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonGeography_BEA.Location = new System.Drawing.Point(0, 80);
+            this.buttonGeography_BEA.Name = "buttonGeography_BEA";
+            this.buttonGeography_BEA.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonGeography_BEA.Size = new System.Drawing.Size(220, 70);
+            this.buttonGeography_BEA.TabIndex = 1;
+            this.buttonGeography_BEA.Text = "   География";
+            this.buttonGeography_BEA.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonGeography_BEA.UseVisualStyleBackColor = true;
+            // 
+            // buttonNature_BEA
+            // 
+            this.buttonNature_BEA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonNature_BEA.FlatAppearance.BorderSize = 0;
+            this.buttonNature_BEA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNature_BEA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonNature_BEA.ForeColor = System.Drawing.Color.Gainsboro;
+            this.buttonNature_BEA.Image = ((System.Drawing.Image)(resources.GetObject("buttonNature_BEA.Image")));
+            this.buttonNature_BEA.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonNature_BEA.Location = new System.Drawing.Point(0, 150);
+            this.buttonNature_BEA.Name = "buttonNature_BEA";
+            this.buttonNature_BEA.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonNature_BEA.Size = new System.Drawing.Size(220, 70);
+            this.buttonNature_BEA.TabIndex = 2;
+            this.buttonNature_BEA.Text = "   Природа";
+            this.buttonNature_BEA.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonNature_BEA.UseVisualStyleBackColor = true;
+            // 
+            // buttonPopulation_BEA
+            // 
+            this.buttonPopulation_BEA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonPopulation_BEA.FlatAppearance.BorderSize = 0;
+            this.buttonPopulation_BEA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPopulation_BEA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPopulation_BEA.ForeColor = System.Drawing.Color.Gainsboro;
+            this.buttonPopulation_BEA.Image = ((System.Drawing.Image)(resources.GetObject("buttonPopulation_BEA.Image")));
+            this.buttonPopulation_BEA.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonPopulation_BEA.Location = new System.Drawing.Point(0, 220);
+            this.buttonPopulation_BEA.Name = "buttonPopulation_BEA";
+            this.buttonPopulation_BEA.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonPopulation_BEA.Size = new System.Drawing.Size(220, 70);
+            this.buttonPopulation_BEA.TabIndex = 3;
+            this.buttonPopulation_BEA.Text = "   Население";
+            this.buttonPopulation_BEA.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonPopulation_BEA.UseVisualStyleBackColor = true;
+            // 
+            // buttonEconomy_BEA
+            // 
+            this.buttonEconomy_BEA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonEconomy_BEA.FlatAppearance.BorderSize = 0;
+            this.buttonEconomy_BEA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEconomy_BEA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEconomy_BEA.ForeColor = System.Drawing.Color.Gainsboro;
+            this.buttonEconomy_BEA.Image = ((System.Drawing.Image)(resources.GetObject("buttonEconomy_BEA.Image")));
+            this.buttonEconomy_BEA.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonEconomy_BEA.Location = new System.Drawing.Point(0, 290);
+            this.buttonEconomy_BEA.Name = "buttonEconomy_BEA";
+            this.buttonEconomy_BEA.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonEconomy_BEA.Size = new System.Drawing.Size(220, 70);
+            this.buttonEconomy_BEA.TabIndex = 4;
+            this.buttonEconomy_BEA.Text = "   Экономика";
+            this.buttonEconomy_BEA.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonEconomy_BEA.UseVisualStyleBackColor = true;
+            // 
+            // buttonPolitics_BEA
+            // 
+            this.buttonPolitics_BEA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonPolitics_BEA.FlatAppearance.BorderSize = 0;
+            this.buttonPolitics_BEA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPolitics_BEA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPolitics_BEA.ForeColor = System.Drawing.Color.Gainsboro;
+            this.buttonPolitics_BEA.Image = ((System.Drawing.Image)(resources.GetObject("buttonPolitics_BEA.Image")));
+            this.buttonPolitics_BEA.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonPolitics_BEA.Location = new System.Drawing.Point(0, 360);
+            this.buttonPolitics_BEA.Name = "buttonPolitics_BEA";
+            this.buttonPolitics_BEA.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonPolitics_BEA.Size = new System.Drawing.Size(220, 70);
+            this.buttonPolitics_BEA.TabIndex = 5;
+            this.buttonPolitics_BEA.Text = "   Политика";
+            this.buttonPolitics_BEA.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonPolitics_BEA.UseVisualStyleBackColor = true;
+            // 
+            // buttonFact_BEA
+            // 
+            this.buttonFact_BEA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonFact_BEA.FlatAppearance.BorderSize = 0;
+            this.buttonFact_BEA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonFact_BEA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFact_BEA.ForeColor = System.Drawing.Color.Gainsboro;
+            this.buttonFact_BEA.Image = ((System.Drawing.Image)(resources.GetObject("buttonFact_BEA.Image")));
+            this.buttonFact_BEA.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonFact_BEA.Location = new System.Drawing.Point(0, 430);
+            this.buttonFact_BEA.Name = "buttonFact_BEA";
+            this.buttonFact_BEA.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.buttonFact_BEA.Size = new System.Drawing.Size(220, 70);
+            this.buttonFact_BEA.TabIndex = 6;
+            this.buttonFact_BEA.Text = "    Факты";
+            this.buttonFact_BEA.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonFact_BEA.UseVisualStyleBackColor = true;
+            // 
+            // panelTitleBar_BEA
+            // 
+            this.panelTitleBar_BEA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.panelTitleBar_BEA.Controls.Add(this.labelTitle_BEA);
+            this.panelTitleBar_BEA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar_BEA.Location = new System.Drawing.Point(220, 0);
+            this.panelTitleBar_BEA.Name = "panelTitleBar_BEA";
+            this.panelTitleBar_BEA.Size = new System.Drawing.Size(865, 80);
+            this.panelTitleBar_BEA.TabIndex = 1;
+            // 
+            // labelTitle_BEA
+            // 
+            this.labelTitle_BEA.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelTitle_BEA.AutoSize = true;
+            this.labelTitle_BEA.Font = new System.Drawing.Font("Mongolian Baiti", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTitle_BEA.ForeColor = System.Drawing.Color.White;
+            this.labelTitle_BEA.Location = new System.Drawing.Point(387, 21);
+            this.labelTitle_BEA.Name = "labelTitle_BEA";
+            this.labelTitle_BEA.Size = new System.Drawing.Size(99, 32);
+            this.labelTitle_BEA.TabIndex = 0;
+            this.labelTitle_BEA.Text = "HOME";
+            // 
+            // FormMainMenu_BEA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 644);
+            this.Controls.Add(this.panelTitleBar_BEA);
+            this.Controls.Add(this.panelMenu_BEA);
+            this.Name = "FormMainMenu_BEA";
+            this.Text = "FormMainMenu";
+            this.panelMenu_BEA.ResumeLayout(false);
+            this.panelTitleBar_BEA.ResumeLayout(false);
+            this.panelTitleBar_BEA.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu_BEA;
+        private System.Windows.Forms.Button buttonFact_BEA;
+        private System.Windows.Forms.Button buttonPolitics_BEA;
+        private System.Windows.Forms.Button buttonEconomy_BEA;
+        private System.Windows.Forms.Button buttonPopulation_BEA;
+        private System.Windows.Forms.Button buttonNature_BEA;
+        private System.Windows.Forms.Button buttonGeography_BEA;
+        private System.Windows.Forms.Panel panelLogo_BEA;
+        private System.Windows.Forms.Panel panelTitleBar_BEA;
+        private System.Windows.Forms.Label labelTitle_BEA;
+    }
+}
+
---
+++ Tyuiu.BelovaEA.Sprint7.Project.V13/FormMainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tyuiu.BelovaEA.Sprint7.Project.V13
+{
+    public partial class FormMainMenu_BEA : Form
+    {
+        public FormMainMenu_BEA()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
